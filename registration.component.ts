18d91452df4baf64989b2d9ddd5f2c74a22dcb11
Subramanian Muthu kumar <100766028+Subramanian04051@users.noreply.github.com>
2022-11-25 17:09:12 +0530

    Add files via upload

--- registration.component.ts
+++ registration.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+interface StudentData
+{
+  name:string;
+  dept:string;
+  state:string;
+  mail:string;
+}
 
 @Component({
   selector: 'app-registration',
@@ -6,12 +13,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-   name:string='';
-   Dept:string='';
-   mailid:string='';
-   state:string='';
-  constructor() { }
 
+  constructor() { }
+  //datarr=new Array(3);
+   jsonObj={};
+  
+  PrintValue()
+  {
+    
+    var attributearr=['name','dept','state','mail']
+     // 👇️ const input: HTMLInputElement | null
+     for(let i=0;i<4;i++)
+     {
+let value= document.getElementById(attributearr[i]) as HTMLInputElement | null;
+  this.jsonObj[attributearr[i]]=value?.value;
+  console.log(this.jsonObj[attributearr[i]]);
+     }
+     
+  }
+ 
+  students:StudentData[]=[{name:this.jsonObj[0],dept:this.jsonObj[1],state:this.jsonObj[2],mail:this.jsonObj[3]}]
   ngOnInit(): void {
   }
 
